983c8039808ca89ba49357b6b7242b95ad8fa70b
vaidotasgarbstas <vaidotas.garbstas@gmail.com>
2022-01-26 11:56:17 +0200

    FOR

--- i.js
+++ i.js
@@ -4,11 +4,75 @@ function rand(min, max) {
     return Math.floor(Math.random() * (max - min) + min);
 }
 
-let kauliukas;
-let kiek = 0;
-do {
-    kauliukas = rand(1, 6);
-    console.log(kauliukas);
-    kiek++
-} while (kauliukas != 2 && kauliukas != 5);
-console.log("Kauliukas ismestas " + kiek + " kartus")
+// for (let aukstas = 0; aukstas < 3; aukstas++) {
+//     console.log('Afrika');
+// }
+
+// for (let aukstas = 0; aukstas < 3; aukstas++) {
+//     console.log('Aukstas: ', aukstas);
+// }
+
+// let kibiras = 0;
+// for (let i = 0; i < 20; i++) {
+//     let siuksliu = rand(5, 105);
+//     kibiras += siuksliu; // kibiras=kibiras + siuksliu;
+// }
+// console.log(kibiras);
+
+// let kibiras = "";
+// for (let i = 0; i < 1000; i++) {
+//     let zvaig = "*";
+//     kibiras += zvaig;
+// }
+// console.log(kibiras);
+
+// let kibiras2 = 0;
+// let kiek = 0;
+// for (let i = 0; i < 20; i++) {
+//     let siuksliu = rand(5, 105);
+//     if (siuksliu > 50) {
+//         kibiras2 += siuksliu;
+//         kiek++
+//     }
+// }
+// console.log(kibiras2);
+// console.log("Kiek aukstu: " + kiek)
+
+
+// let kibiras3 = 0;
+// let kiek = 0;
+// for (let i = 0; i < 20; i++) {
+//     let siuksliu = rand(5, 105);
+//     if (siuksliu > 50) {
+//         kibiras3 += siuksliu;
+//         kiek++
+//     }
+// }
+// console.log(kibiras3);
+// console.log("Kiek aukstu: " + kiek)
+
+
+// let kibirasBlue = 0;
+// let kibirasGreen = 0;
+
+// for (let i = 0; i < 20; i++) {
+//     let siuksliu = rand(5, 105);
+//     if (i % 2 == 0) {
+//         kibirasBlue += siuksliu;
+//     } else {
+//         kibirasGreen += siuksliu;
+//     }
+// }
+// console.log(kibirasBlue, kibirasGreen);
+
+
+let kibiras = 0;
+for (let i = 0; i < 20; i++) {
+    let siuksliu = rand(5, 105);
+    if (kibiras + siuksliu > 500) {
+        break;
+    } else {
+        kibiras += siuksliu;
+    }
+}
+console.log(kibiras);
